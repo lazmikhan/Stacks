fe99b7af5653bc013855d646f30a2705ecf9f009
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2022-09-08 06:00:33 +0600

    Delete tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-  stacks.push1(2);
-        stacks.push2(3);
-        stacks.push2(4);
-        stacks.push2(8);
-        stacks.push1(33);
-        stacks.push2(80);
-        stacks.push2(80);
-        stacks.push1(7);
